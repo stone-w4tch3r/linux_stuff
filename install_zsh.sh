1aa8ca3e80dce3e10ac9f519a1bae0c6617b3877
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-21 01:09:17 +0500

    added git_cp + shellcheck suggestions

--- install_zsh.sh
+++ install_zsh.sh
@@ -5,13 +5,15 @@
 sudo apt install -y git-core zsh fzf zsh-syntax-highlighting zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-chsh -s $(which zsh)
-sudo chsh -s $(which zsh)
+chsh -s "$(which zsh)"
+sudo chsh -s "$(which zsh)"
 
 sed -i 's|plugins=.*|plugins=(git dotnet adb ag ansible battery cake chucknorris colored-man-pages colorize command-not-found docker docker-compose emoji emoji-clock extract gh gitignore hitchhiker multipass npm  pip safe-paste sudo ubuntu ufw zsh-interactive-cd)|g' ~/.zshrc
 echo 'alias gitignore=gi' | tee -a ~/.zshrc
 echo 'alias cat=ccat' | tee -a ~/.zshrc
 echo 'alias less=cless' | tee -a ~/.zshrc
+echo "alias git_cp='echo -n "commit message: " && read -r message && echo \$message | git add . && git commit -m \$message && git push'" \
+    | tee -a ~/.zshrc
 echo 'zic_case_insensitive=true' | sudo tee -a /etc/environment #case-insensitive interactive cd
 
 wget -P \
@@ -22,6 +24,6 @@ wget -P \
 sudo mv ~/fonts_for_zsh/* /usr/local/share/fonts
 rm -rfd ~/fonts_for_zsh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 sed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
 cp configs/p10k.zsh ~/.p10k.zsh
