fb6e8e2fa8382196271b4c85e17e7ae24895095e
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-21 01:20:11 +0500

    small cleanup (aliases)

--- install_zsh.sh
+++ install_zsh.sh
@@ -9,11 +9,6 @@ chsh -s "$(which zsh)"
 sudo chsh -s "$(which zsh)"
 
 sed -i 's|plugins=.*|plugins=(git dotnet adb ag ansible battery cake chucknorris colored-man-pages colorize command-not-found docker docker-compose emoji emoji-clock extract gh gitignore hitchhiker multipass npm  pip safe-paste sudo ubuntu ufw zsh-interactive-cd)|g' ~/.zshrc
-echo 'alias gitignore=gi' | tee -a ~/.zshrc
-echo 'alias cat=ccat' | tee -a ~/.zshrc
-echo 'alias less=cless' | tee -a ~/.zshrc
-echo "alias git_cp='echo -n "commit message: " && read -r message && echo \$message | git add . && git commit -m \$message && git push'" \
-    | tee -a ~/.zshrc
 echo 'zic_case_insensitive=true' | sudo tee -a /etc/environment #case-insensitive interactive cd
 
 wget -P \
@@ -27,3 +22,9 @@ rm -rfd ~/fonts_for_zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 sed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
 cp configs/p10k.zsh ~/.p10k.zsh
+
+echo 'alias gitignore=gi' | tee -a ~/.zshrc
+echo 'alias cat=ccat' | tee -a ~/.zshrc
+echo 'alias less=cless' | tee -a ~/.zshrc
+echo "alias git_cp='echo -n "commit message: " && read -r message && echo \$message | git add . && git commit -m \$message && git push'" \
+    | tee -a ~/.zshrc
