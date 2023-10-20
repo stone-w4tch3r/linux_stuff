a6f0a9f5b989d05f6cf431d494197e12eba6dd4e
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-20 18:28:09 +0500

    alias cless

--- install_zsh.sh
+++ install_zsh.sh
@@ -11,6 +11,7 @@ sudo chsh -s $(which zsh)
 sed -i 's|plugins=.*|plugins=(git dotnet adb ag ansible battery cake chucknorris colored-man-pages colorize command-not-found docker docker-compose emoji emoji-clock extract gh gitignore hitchhiker multipass npm  pip safe-paste sudo ubuntu ufw zsh-interactive-cd)|g' ~/.zshrc
 echo 'alias gitignore=gi' | tee -a ~/.zshrc
 echo 'alias cat=ccat' | tee -a ~/.zshrc
+echo 'alias less=cless' | tee -a ~/.zshrc
 echo 'zic_case_insensitive=true' | sudo tee -a /etc/environment #case-insensitive interactive cd
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
