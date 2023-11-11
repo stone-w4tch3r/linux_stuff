d6e12b73176810ac3751c785d3134a78c01249e3
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-11 13:48:08 +0500

    1. moved all code to main.py 2. added p10.zsh-s 3. fixed files.block bug 4. lines reordering

--- pyinfra_manager/deploys/shell/main.py
+++ pyinfra_manager/deploys/shell/main.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
 from pyinfra import host
-from pyinfra.facts import server
+from pyinfra.facts import files as facts_files
+from pyinfra.facts import server as facts_server
+from pyinfra.operations import apt, server, files, git, python
 
-from deploys.shell.deployer import deploy
 from deploys.shell.vars import shell_vars
 
 
@@ -12,16 +13,57 @@ class ShellComplexity(Enum):
     Extended = 1
 
 
-def prepare_params(shell_complexity: ShellComplexity) -> tuple[list[str], str, str]:
-    return (
-        shell_vars.Packages,
-        (" ".join(shell_vars.ZshPluginsBasic)
-         if shell_complexity == ShellComplexity.Basic
-         else " ".join(shell_vars.ZshPluginsBasic + shell_vars.ZshPluginsExtended)),
-        f"/home/{host.get_fact(server.User)}",
-    )
+def is_ohmyzsh_installed(home_path: str) -> bool:
+    return host.get_fact(facts_files.Directory, f"{home_path}/.oh-my-zsh")
+
+
+def get_current_powerlevel_block_content(home_path: str) -> list[str]:
+    return host.get_fact(facts_files.Block, path=f"{home_path}/.zshrc", marker="#### {mark} POWERLEVEL BLOCK ####")
 
 
 def deploy_shell(shell_complexity: ShellComplexity) -> None:
-    params = prepare_params(shell_complexity)
-    deploy(*params)
+    packages = shell_vars.Packages
+    plugins_str = " ".join(shell_vars.ZshPluginsBasic) \
+        if shell_complexity == ShellComplexity.Basic \
+        else " ".join(shell_vars.ZshPluginsBasic + shell_vars.ZshPluginsExtended)
+    home_path = f"/home/{host.get_fact(facts_server.User)}"
+    fonts_links = shell_vars.FontsLinks
+    p10k_to_put = "deploys/shell/files/p10k_server.zsh" \
+        if shell_complexity == ShellComplexity.Basic \
+        else "deploys/shell/files/p10k_extended.zsh"
+    powerlevel_block_content = [
+        '# To customize prompt, run "p10k configure" or edit ~/.p10k.zsh.',
+        '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh',
+    ]
+
+    apt.update(cache_time=86400, _sudo=True)
+    apt.packages(packages=packages, _sudo=True)
+
+    server.user(user=host.get_fact(facts_server.User), shell="/usr/bin/zsh", _sudo=True)
+
+    if not is_ohmyzsh_installed(home_path):
+        server.shell(commands=['sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'])
+
+    git.repo(src="https://github.com/romkatv/powerlevel10k", dest=f"{home_path}/.oh-my-zsh/custom/themes/powerlevel10k", pull=False)
+    files.put(src=p10k_to_put, dest=f"{home_path}/.p10k.zsh", mode="0644")
+
+    files.line(path="/etc/environment", line="zic_case_insensitive=true", _sudo=True)
+
+    files.line(path=f"{home_path}/.zshrc", replace=f"plugins=({plugins_str})", line="^plugins=.*")
+    files.line(path=f"{home_path}/.zshrc", replace="ZSH_THEME=\"powerlevel10k/powerlevel10k\"", line="^ZSH_THEME=.*")
+
+    # ugly hack to bypass pyinfra two-step model. Wait for v3 to fix it
+    if powerlevel_block_content != get_current_powerlevel_block_content(home_path):
+        python.call(
+            function=lambda: files.block(
+                path=f"{home_path}/.zshrc",
+                content=powerlevel_block_content,
+                marker="#### {mark} POWERLEVEL BLOCK ####",
+            )
+        )
+
+    if shell_complexity == ShellComplexity.Extended:
+        files.directory(path="/usr/share/fonts/truetype/", present=True, _sudo=True)
+        for link in fonts_links:
+            filename = link.split("/")[-1].replace("%20", " ")
+            files.download(src=link, dest=f"/usr/share/fonts/truetype/{filename}", mode="0644", _sudo=True)
