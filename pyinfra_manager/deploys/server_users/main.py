166550d3a1774afce5072cf7240dc115a400162e
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-23 19:19:54 +0500

    started server_users

---
+++ pyinfra_manager/deploys/server_users/main.py
@@ -0,0 +1,5 @@
+
+
+# creates new_user with new_user_password, adds new_user to sudoers
+# what else???
+def 
--- pyinfra_manager/inventory.py
+++ pyinfra_manager/inventory.py
@@ -5,7 +5,9 @@ my_hosts = [
             "ssh_user": "ubuntu",
             "ssh_password": "ubuntu",
             "_use_sudo_password": "ubuntu",
-            "shell_complexity": "Basic"
+            "shell_complexity": "Basic",
+            "new_user": "lord",
+            "new_user_password": "r4ndmP4ssw0rd",
         }
     ),
 ]
