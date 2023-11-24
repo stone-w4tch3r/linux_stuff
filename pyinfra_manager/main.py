ddb8d0a8f61a868d2636c198589c37982b6a3592
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-25 00:11:36 +0500

    new function to main

--- pyinfra_manager/main.py
+++ pyinfra_manager/main.py
@@ -1,5 +1,6 @@
 from deploys.server_upgrade.main import server_upgrade
 from deploys.server_user.main import deploy_server_user
+from deploys.shell_aliases.main import deploy_aliases
 from deploys.zsh.main import deploy_zsh, ShellComplexity
 from pyinfra import host
 from pyinfra.operations import apt
@@ -9,4 +10,5 @@ from deploys.shell_tools.main import deploy_shell_tools
 # server_upgrade()
 # deploy_zsh()
 # deploy_shell_tools()
-deploy_server_user()
+# deploy_server_user()
+deploy_aliases()
