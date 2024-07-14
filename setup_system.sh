728fecfa85b69f87a715261d28875fad12572917
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-07-14 19:14:14 +0500

    ipv6 disable

--- setup_system.sh
+++ setup_system.sh
@@ -442,10 +442,16 @@ sudo sed -i "/commit/i \ \ $CONFIG_LINE5" "$CONFIG_PATH"
 #disable ipv6
 ####
 
+# temp
 echo 1 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 | sudo tee /proc/sys/net/ipv6/conf/default/disable_ipv6
-echo "net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee /etc/sysctl.d/98-disable-ipv6.conf
+
+# permanent
+sudo tee /etc/sysctl.d/98-disable-ipv6.conf <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
 
 ####
 #latest yt-dlp
